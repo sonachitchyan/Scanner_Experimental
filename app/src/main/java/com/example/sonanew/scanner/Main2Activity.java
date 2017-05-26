2593498260ece836c7ac86f473753b2d2d280269
Sona Chitchyan <chitchyan.sona@gmail.com>
2017-05-26 15:37:32 +0400

    inner class commit

--- app/src/main/java/com/example/sonanew/scanner/Main2Activity.java
+++ app/src/main/java/com/example/sonanew/scanner/Main2Activity.java
@@ -14,13 +14,14 @@ public class Main2Activity extends AppCompatActivity {
     private ListView listView;
     private ArrayList<String> arrayList;
     private ArrayAdapter<String> arrayAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         listView = (ListView) findViewById(R.id.list);
         Gson gson = new Gson();
-        arrayList = gson.fromJson(getIntent().getStringExtra("info"),(Type) new Data());
+        arrayList = gson.fromJson(getIntent().getStringExtra("info"), (Type) new Data());
         arrayAdapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, arrayList);
         listView.setAdapter(arrayAdapter);
     }
--- app/src/main/java/com/example/sonanew/scanner/MainActivity.java
+++ app/src/main/java/com/example/sonanew/scanner/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.sonanew.scanner;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.KeyListener;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
@@ -19,9 +23,10 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button add, scan;
+    //private Button add, scan;
     private EditText barcode, count;
     private List<Data> list = new ArrayList<>();
+    private BroadcastReceiver scanner = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,43 +34,63 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         barcode = (EditText) findViewById(R.id.barcode);
         count = (EditText) findViewById(R.id.count);
-        count.setFocusable(true);
-        count.setFocusableInTouchMode(true);
-        barcode.setFocusable(false);
-        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.line);
-        linearLayout.requestFocus();
-        add = (Button) findViewById(R.id.add);
-        scan = (Button) findViewById(R.id.scan);
-        read_barcode();
-
-
-    }
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        switch (keyCode) {
-            case KeyEvent.KEYCODE_ENTER:
-                if (!barcode.getText().toString().equals("") && !count.getText().toString().equals("")) {
-                    Data data = new Data(barcode.getText().toString(), Integer.parseInt(count.getText().toString()));
-                    list.add(data);
-                    Gson gson = new Gson();
-                    String info = gson.toJson(list);
-                    Intent intent = new Intent(MainActivity.this, Main2Activity.class);
-                    intent.putExtra("info", info);
-                    startActivity(intent);
-                }
+        View view = getCurrentFocus();
+        switch (view.getId()) {
+            case R.id.barcode:
+                view.clearFocus();
                 break;
         }
-        return true;
+        barcode.setFocusable(false);
+//        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.line);
+//        linearLayout.requestFocus();
+//        add = (Button) findViewById(R.id.add);
+//        scan = (Button) findViewById(R.id.scan);
+        scanner = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String bar = intent.getStringExtra("SCAN_BARCODE1");
+                String scanStatus = intent.getStringExtra("SCAN_STATE");
+                if ("ok".equals(scanStatus)) {
+                    barcode.setText(bar);
+                } else {
+                    Toast.makeText(context, "Try again", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+        IntentFilter intentFilter = new IntentFilter("nlscan.action.SCANNER_RESULT");
+        this.registerReceiver(scanner, intentFilter);
+        count.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                    Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                    Gson gson = new Gson();
+                    String info = gson.toJson(list);
+                    intent.putExtra("info",info);
+                    startActivity(intent);
+                }
+                return true;
+            }
+        });
+
+
     }
 
-    public void read_barcode() {
-        Intent intent = new Intent("nlscan.action.SCANNER_RESULT");
-        sendBroadcast(intent);
-        intent = getIntent();
-        barcode.setText(intent.getStringExtra("barcode"));
-        if (!barcode.getText().toString().equals("")) {
-            count.requestFocus();
-        }
-    }
+//    @Override
+//    public boolean onKeyDown(int keyCode, KeyEvent event) {
+//        switch (keyCode) {
+//            case KeyEvent.KEYCODE_ENTER:
+//                if (!barcode.getText().toString().equals("") && !count.getText().toString().equals("")) {
+//                    Data data = new Data(barcode.getText().toString(), Integer.parseInt(count.getText().toString()));
+//                    list.add(data);
+//                    Gson gson = new Gson();
+//                    String info = gson.toJson(list);
+//                    Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+//                    intent.putExtra("info", info);
+//                    startActivity(intent);
+//                }
+//                break;
+//        }
+//        return true;
+//    }
 }
